perXautomatik/ScoopBucket

commit 85a3bd94304ca7ce82062bf80e2a7a5c83e35ae9

Fix robocopy not releasing a directory after moving it (closes #2305)
- Use System.Diagnostics.Process for better control
- Wait up to 1 second after moving a directory, so robocopy can terminate its threads

lib/core.ps1
@@ -244,9 +244,28 @@ function movedir($from, $to) {
     $from = $from.trimend('\')
     $to = $to.trimend('\')
 
-    $out = robocopy "$from" "$to" /e /move
-    if($lastexitcode -ge 8) {
-        throw "Could not find '$(fname $from)'! (error $lastexitcode)"
+    $proc = New-Object System.Diagnostics.Process
+    $proc.StartInfo.FileName = 'robocopy.exe'
+    $proc.StartInfo.Arguments = "$from $to /e /move"
+    $proc.StartInfo.RedirectStandardOutput = $true
+    $proc.StartInfo.RedirectStandardError = $true
+    $proc.StartInfo.UseShellExecute = $false
+    $proc.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+    $proc.Start()
+    $out = $proc.StandardOutput.ReadToEnd()
+    $proc.WaitForExit()
+
+    if($proc.ExitCode -ge 8) {
+        debug $out
+        throw "Could not find '$(fname $from)'! (error $($proc.ExitCode))"
+    }
+
+    # wait for robocopy to terminate its threads
+    1..10 | ForEach-Object {
+        if (!(Test-Path $from)) {
+            break
+        }
+        Start-Sleep -Milliseconds 100
     }
 }
 

commit a86aca1779bc71d30cd6fe0be70f0b9c1106afb9

fix a bug introduced with 85a3bd94
"break" prevented the extraction of multiple archive files

lib/core.ps1
@@ -262,10 +262,9 @@ function movedir($from, $to) {
 
     # wait for robocopy to terminate its threads
     1..10 | ForEach-Object {
-        if (!(Test-Path $from)) {
-            break
+        if (Test-Path $from) {
+            Start-Sleep -Milliseconds 100
         }
-        Start-Sleep -Milliseconds 100
     }
 }
 