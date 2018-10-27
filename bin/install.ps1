perXautomatik/ScoopBucket

commit 362c6f72209c0b37487f6ee466d2337e65953106

Fix the check for ExecutionPolicy scope (#2703)
Under certain terminals like cmder, `Get-ExecutionPolicy` will show the scope of `Process`.  This fix explicitly checks the scope of `CurrentUser`.

Signed-off-by: Ben Dang <me@bdang.it>

bin/install.ps1
@@ -12,7 +12,7 @@ if(($PSVersionTable.PSVersion.Major) -lt 3) {
 }
 
 # show notification to change execution policy:
-if((get-executionpolicy) -gt 'RemoteSigned') {
+if((get-executionpolicy -scope CurrentUser) -gt 'RemoteSigned') {
     Write-Output "PowerShell requires an execution policy of 'RemoteSigned' to run Scoop."
     Write-Output "To make this change please run:"
     Write-Output "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"

commit 75f1c60f08cb61217ffa951c0cd0fce579164be8

Fix ExecutionPolicy regression introduced with #2703

bin/install.ps1
@@ -12,7 +12,7 @@ if(($PSVersionTable.PSVersion.Major) -lt 3) {
 }
 
 # show notification to change execution policy:
-if((get-executionpolicy -scope CurrentUser) -gt 'RemoteSigned') {
+if((Get-ExecutionPolicy) -gt 'RemoteSigned' -or (Get-ExecutionPolicy) -eq 'ByPass') {
     Write-Output "PowerShell requires an execution policy of 'RemoteSigned' to run Scoop."
     Write-Output "To make this change please run:"
     Write-Output "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"